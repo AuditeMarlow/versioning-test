e88d1973ea1bb60b4a9b14580a7b98a6a33bfe45
Niek den Breeje <n.j.den.breeje@gmail.com>
2025-03-12 18:04:58 +0100

    feat: update app1

--- app1/main.go
+++ app1/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <name>")
+		return
+	}
+
+	fmt.Printf("Goodbye, %s!\n", os.Args[1])
 }
